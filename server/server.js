a2f682c42c84da4829d9adaaed8090d91e2cc728
solutionsolar <122757410+solutionsolar@users.noreply.github.com>
2023-01-15 19:05:04 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@ import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
 
-console.log(process.env.OPENAI_API_KEY);
-process.env.OPENAI_API_KEY = ""
-console.log(process.env.OPENAI_API_KEY);
+//console.log(process.env.OPENAI_API_KEY);
+
+
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
